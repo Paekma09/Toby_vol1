dbd1b733f2ae1107787758593d5b22435f66f175
KJ Nam <paekma09@gmail.com>
2023-06-09 00:29:15 +0900

    1-7-5

--- src/springbook/user/dao/DaoFactory.java
+++ src/springbook/user/dao/DaoFactory.java
@@ -6,9 +6,21 @@ import org.springframework.context.annotation.Bean;
 @Configuration
 public class DaoFactory {
     // ConnectionMaker 타입의 오브젝트를 결정하고 이를 생성한 후에 UserDao의 생성자 파라미터로 주입
+
+    /*
+    // 생성자를 이용한 의존관계 주입
     @Bean
     public UserDao userDao(){
         return new UserDao(connectionMaker());
+    }
+    */
+
+    // 수정자를 이용한 의존관계 주입
+    @Bean
+    public UserDao userDao(){
+        UserDao userDao = new UserDao();
+        userDao.setConnectionMaker(connectionMaker());
+        return userDao;
     }
 
     @Bean
