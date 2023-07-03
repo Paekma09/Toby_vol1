e659bd02f16d08475fbc46929722c1cb1a4a08cf
KJ Nam <paekma09@gmail.com>
2023-07-03 22:14:58 +0900

    3-2-2

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -9,6 +9,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+//public abstract class UserDao {
 public class UserDao {
     // 생성자 파라미터를 통해 전달 받은 런타임 의존관계를 갖는 오브젝트는 인스턴스 변수에 저장해둔다.
     // private ConnectionMaker connectionMaker;
@@ -90,34 +91,99 @@ public class UserDao {
         return user;
     }
 
-    public void deleteAll() throws SQLException {
+//    public void deleteAll() throws SQLException {
+//        Connection c = null;
+//        PreparedStatement ps = null;
+////        Connection c = dataSource.getConnection();
+////        PreparedStatement ps = c.prepareStatement("delete from users");
+//        try {
+//            c = dataSource.getConnection();
+////            ps = c.prepareStatement("delete from users"); -> 메소드 추출 방식 적용
+////            ps = makeStatement(c); -> 템플릿 메소드 패턴의 적용
+//
+//            // 전략 패턴의 적용
+//            StatementStrategy statementStrategy = new DeleteAllStatement();
+//            ps = statementStrategy.makePrepareStatement(c);
+//
+//            ps.executeUpdate();
+//        } catch (SQLException e) {
+//            throw e;
+//        } finally {
+//            if(ps != null) {
+//                try {
+//                    ps.close();
+//                } catch (SQLException ignored) {
+//                }
+//            }
+//            if(c != null) {
+//                try {
+//                    c.close();
+//                } catch (SQLException e) {
+//                }
+//            }
+//        }
+////        ps.close();
+////        c.close();
+//    }
+
+// 메소드 추출 방식 적용
+//    private PreparedStatement makeStatement(Connection c) throws SQLException {
+//        PreparedStatement ps;
+//        ps = c.prepareStatement("delete from users");
+//        return ps;
+//    }
+
+    // 템플릿 메소드 패턴의 적용 ----[S]
+    // UserDao 추상메소드로 변경 (슈퍼 클래스)
+//    abstract protected PreparedStatement makeStatement(Connection c) throws SQLException;
+
+// 서브 클래스
+//public class UserDaoDeleteAll extends UserDao {
+//
+//    @Override
+//    protected PreparedStatement makeStatement(Connection c) throws SQLException {
+//        PreparedStatement ps = c.prepareStatement("delete from users");
+//        return ps;
+//    }
+//}
+    // 템플릿 메소드 패턴의 적용 ----[E]
+
+
+    // DI 적용을 위한 클라이언트 / 컨텍스트 분리 ----[S]
+    // 컨텍스트
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException { //StatementStrategy stmt -> 클라이언트가 컨텍스트를 호출할 때 넘겨줄 전략 파라미터
         Connection c = null;
         PreparedStatement ps = null;
-//        Connection c = dataSource.getConnection();
-//        PreparedStatement ps = c.prepareStatement("delete from users");
+
         try {
             c = dataSource.getConnection();
-            ps = c.prepareStatement("delete from users");
+            ps = stmt.makePrepareStatement(c);
             ps.executeUpdate();
         } catch (SQLException e) {
             throw e;
         } finally {
-            if(ps != null) {
+            if (ps != null) {
                 try {
                     ps.close();
                 } catch (SQLException e) {
                 }
             }
-            if(c != null) {
+            if (c != null) {
                 try {
                     c.close();
                 } catch (SQLException e) {
                 }
             }
         }
-//        ps.close();
-//        c.close();
     }
+
+    // 클라이언트
+    public void deleteAll() throws SQLException {
+        StatementStrategy st = new DeleteAllStatement();    //선정한 전략 클래스의 오브젝트 생성
+        jdbcContextWithStatementStrategy(st);   //컨텍스트 호출, 전략 오브젝트 전달
+    }
+    // DI 적용을 위한 클라이언트 / 컨텍스트 분리 ----[E]
+
 
     public int getCount() throws SQLException {
 //        Connection c = dataSource.getConnection();
