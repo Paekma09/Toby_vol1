5dcbf63c8cb6d0d334338a767b236eb1bd242a3c
KJ Nam <paekma09@gmail.com>
2023-07-25 23:11:03 +0900

    5-4-2

--- src/main/java/user/service/UserService.java
+++ src/main/java/user/service/UserService.java
@@ -11,10 +11,19 @@ import user.dao.UserDao;
 import user.domain.Level;
 import user.domain.User;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.sql.DataSource;
+import java.io.UnsupportedEncodingException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Properties;
 
 public class UserService {
     // 상수의 도입
@@ -153,6 +162,7 @@ public class UserService {
     protected void upgradeLevel(User user) {
         user.upgradeLevel();
         userDao.update(user);
+        sendUpgradeEMail(user);
     }
 
     // 사용자 신규 등록 로직을 담은 add() 메소드
@@ -198,4 +208,23 @@ public class UserService {
 //            }
 //        }
 //    }
+
+    // JavaMail 을 이용한 메일 발송 메소드
+    private void sendUpgradeEMail(User user) {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        Session s = Session.getInstance(props, null);
+
+        MimeMessage message = new MimeMessage(s);
+
+        try {
+            message.setFrom(new InternetAddress("paekma09@gmail.com"));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
+            message.setSubject("Upgrade 안내");
+            message.setText("사용자님의 등급이  " + user.getLevel().name() + "로 업그레이드 되었습니다.");
+            Transport.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
