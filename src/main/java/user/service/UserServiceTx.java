d69eb43917322b398cc590ff66a66f5df0839318
KJ Nam <paekma09@gmail.com>
2023-07-31 21:40:45 +0900

    6-1-2

---
+++ src/main/java/user/service/UserServiceTx.java
@@ -0,0 +1,44 @@
+package user.service;
+
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionException;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import user.domain.User;
+
+// 위임기능을 가진 UserServiceTx 클래스
+public class UserServiceTx implements UserService{
+
+    // UserService 를 구현한 다른 오브젝트를 DI 받는다.
+    UserService userService;
+    PlatformTransactionManager transactionManager;
+
+    public void setTransactionManager(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    // DI 받은 UserService 오브젝트에 모든 기능을 위임한다.
+    @Override
+    public void add(User user) {
+        userService.add(user);
+    }
+
+    @Override
+    public void upgradeLevels() {
+        TransactionStatus status = this.transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+
+            userService.upgradeLevels();
+
+            this.transactionManager.commit(status);
+        } catch (RuntimeException e) {
+            this.transactionManager.rollback(status);
+            throw e;
+        }
+    }
+}
